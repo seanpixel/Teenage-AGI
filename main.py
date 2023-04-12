85b4184e597f8aacb419427384c30dac6adcf9f5
Mike Kelly <mikekelly321@gmail.com>
2023-04-12 12:16:23 +0100

    make agent name configurable from env variable

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import agent
+import os
 from agent import Agent
+from dotenv import load_dotenv
 
-agent = Agent("myAgent")
+# Load default environment variables (.env)
+load_dotenv()
+
+AGENT_NAME = os.getenv("AGENT_NAME") or "my-agent"
+
+agent = Agent(AGENT_NAME)
 
 # Creates Pinecone Index
 agent.createIndex()
