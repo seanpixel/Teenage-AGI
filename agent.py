b989a563a0ed50b1dab3fcaef87b18b06bfb0a42
seanpixel <112411911+seanpixel@users.noreply.github.com>
2023-04-19 19:07:42 -0700

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,10 +1,12 @@
 import openai
 import os
-import pinecone
+import chromadb
 import yaml
 from dotenv import load_dotenv
 import nltk
 from langchain.text_splitter import NLTKTextSplitter
+
+
 
 # Download NLTK for Reading
 nltk.download('punkt')
@@ -29,10 +31,9 @@ def generate(prompt):
 
     return completion.choices[0].message["content"]
 
-PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-PINECONE_API_ENV = os.getenv("PINECONE_API_ENV")
-#PINECONE_API_ENV = "asia-southeast1-gcp"
+
     
 # Prompt Initialization
 with open('prompts.yaml', 'r') as f:
@@ -56,8 +57,6 @@ ACTIONS = "Actions"
 # Top matches length
 k_n = 3
 
-# initialize pinecone
-pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_API_ENV)
 
 # initialize openAI
 openai.api_key = OPENAI_API_KEY # you can just copy and paste your key here if you want
@@ -70,37 +69,17 @@ def get_ada_embedding(text):
 
 
 class Agent():
-    def __init__(self, table_name=None) -> None:
-        self.table_name = table_name
-        self.memory = None
+    def __init__(self, name) -> None:
+        chroma_client = chromadb.Client()
+        self.memory = chroma_client.create_collection(name)
         self.thought_id_count = int(counter['count'])
         self.last_message = ""
-
-    # Keep Remebering!
-    # def __del__(self) -> None:
-    #     with open('memory_count.yaml', 'w') as f:
-    #         yaml.dump({'count': str(self.thought_id_count)}, f)
-    
-
-    def createIndex(self, table_name=None):
-        # Create Pinecone index
-        if(table_name):
-            self.table_name = table_name
-
-        if(self.table_name == None):
-            return
-
-        dimension = 1536
-        metric = "cosine"
-        pod_type = "p1"
-        if self.table_name not in pinecone.list_indexes():
-            pinecone.create_index(
-                self.table_name, dimension=dimension, metric=metric, pod_type=pod_type
-            )
-
-        # Give memory
-        self.memory = pinecone.Index(self.table_name)
-
+        self.memory.add(
+                    documents=["You are an intelligent agent with thoughts and memories. You have a memory which stores your past thoughts and actions and also how other users have interacted with you."],
+                    metadatas=[{"thought_type" : THOUGHTS}],
+                    ids=[str(self.thought_id_count)]
+                )
+        
     
     # Adds new Memory to agent, types are: THOUGHTS, ACTIONS, QUERIES, INFORMATION
     def updateMemory(self, new_thought, thought_type):
@@ -119,29 +98,46 @@ class Agent():
             # Not needed since already in prompts.yaml as external thought memory
             pass
 
-        vector = get_ada_embedding(new_thought)
-        upsert_response = self.memory.upsert(
-        vectors=[
-            {
-            'id':f"thought-{self.thought_id_count}", 
-            'values':vector, 
-            'metadata':
-                {"thought_string": new_thought
-                }
-            }],
-	    namespace=thought_type,
+        self.memory.add(
+            documents=[new_thought],
+            metadatas=[{"thought_type": thought_type}],
+            ids=[str(self.thought_id_count)]
         )
 
         self.thought_id_count += 1
 
     # Agent thinks about given query based on top k related memories. Internal thought is passed to external thought
     def internalThought(self, query) -> str:
-        query_embedding = get_ada_embedding(query)
-        query_results = self.memory.query(query_embedding, top_k=2, include_metadata=True, namespace=QUERIES)
-        thought_results = self.memory.query(query_embedding, top_k=2, include_metadata=True, namespace=THOUGHTS)
-        results = query_results.matches + thought_results.matches
-        sorted_results = sorted(results, key=lambda x: x.score, reverse=True)
-        top_matches = "\n\n".join([(str(item.metadata["thought_string"])) for item in sorted_results])
+        n = k_n
+        if k_n > self.memory.count():
+            n = self.memory.count()
+
+        results = []
+
+        try:
+            query_results = self.memory.query(
+                query_texts=[query],
+                n_results=n,
+                where={"thought_type": QUERIES}
+            )
+            results += query_results["documents"][0]
+        except:
+            pass
+
+        if n > self.memory.count():
+            n = self.memory.count()
+
+        try:
+            thought_results = self.memory.query(
+                query_texts=[query],
+                n_results=n,
+                where={"thought_type": THOUGHTS}
+            )
+            results += thought_results["documents"][0]
+        except:
+            pass
+
+        top_matches = "\n\n".join(results) # WIP
         #print(top_matches)
         
         internalThoughtPrompt = data['internal_thought']
@@ -183,22 +179,20 @@ class Agent():
     # Make agent read some information
     def read(self, text) -> str:
         texts = text_splitter.split_text(text)
-        vectors = []
-        for t in texts:
-            t = "This is information fed to you by the user:\n" + t
-            vector = get_ada_embedding(t)
-            vectors.append({
-                'id':f"thought-{self.thought_id_count}", 
-                'values':vector, 
-                'metadata':
-                    {"thought_string": t, 
-                     }
-                })
+
+        metadatas = []
+        counted_ids = []
+
+        for i in range(0, len(texts)):
+            texts[i] = "This is information fed to you by the user:\n" + texts[i]
+            metadatas.append({"thought_type", INFORMATION})
+            counted_ids.append(str(self.thought_id_count))
             self.thought_id_count += 1
 
-        upsert_response = self.memory.upsert(
-        vectors,
-	    namespace=INFORMATION,
+        self.memory.add(
+            texts,
+            metadatas=metadatas,
+            ids=counted_ids
         )
 
 
